988180c847e311b4a9d089f5b24ee8ae994d49f8
loralie flint <loralie.flint0128@gmail.com>
2020-11-17 19:22:10 -0800

    graphs 2 mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,4 +1,17 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
+# notes from graphs 3 lecture 
 def dfs(ancestors, node, distance):
     parent = getNeighbors(node)
     
@@ -16,6 +29,24 @@ def dfs(ancestors, node, distance):
 
 
 def earliest_ancestor(ancestors, starting_node):
-    ancient_one = dfs(ancestors, starting_node, 0)
+    queue = Queue()
+    path = [starting_node]
+    queue.enqueue(path)
+    visited = set()
+    while queue.size() > 0:
 
-    return ancient_one
+        current_pair = queue.dequeue()
+        neighbor = []
+
+        for i in current_pair:
+            for pairs in ancestors:
+                if pairs[1] == i and i not in visited:
+                    visited.add(i)
+                    neighbor.append(pairs[0])
+                    queue.enqueue(neighbor)
+        
+        if len(neighbor) <= 0:
+            if current_pair[0] == starting_node:
+                return -1
+            else:
+                return current_pair[0]
