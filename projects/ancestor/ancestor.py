7379055f7bafb9d09d9819be585de76a52ecd7f7
loralie flint <loralie.flint0128@gmail.com>
2020-11-17 18:17:07 -0800

    graphs day 2 project starting

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
 
+def dfs(ancestors, node, distance):
+    parent = getNeighbors(node)
+    
+    if len(parents) == 0:
+        return (node, distance)
+
+    ancient_one = (node, distance)
+    for parent in parents:
+        node_pair = dfs(ancestors, parent, distance)
+
+        if node_pair[i] > distance:
+            ancient_one = node_pair
+
+    return ancient_one
+
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    ancient_one = dfs(ancestors, starting_node, 0)
+
+    return ancient_one
