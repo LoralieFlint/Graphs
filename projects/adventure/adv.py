36e50302fd604f86a24a42d143fe85e38e471327
loralie flint <loralie.flint0128@gmail.com>
2020-11-20 18:02:38 -0800

    trying to run python to make sure it work but on new pc having issues

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -30,22 +30,41 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
-
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
+opposites = {'n':'s', 's':'n', 'e':'w', 'w':'e'}
+traversal_graph = {}
+
+def traverse_graph():
+    moves = []
+    traversal_graph[player.current_room.id] = {}
+    for cur_direction in player.current_room.get_exits():
+        prev = player.current_room.id
+        player.travel(cur_direction)
+        traversal_graph[prev][cur_direction] = player.current_room.id
+        if player.current_room in visited_rooms:
+            player.travel(opposites[cur_direction])
+        else:
+            visited_rooms.add(player.current_room)
+            moves.append(cur_direction)
+            moves.extend(traverse_graph())
+            player.travel(opposites[cur_direction])
+            moves.append(opposites[cur_direction])
+    return moves
+
+
+traversal_path = traverse_graph()
+
+
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
